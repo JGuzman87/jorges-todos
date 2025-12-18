56f9362b9fc52c0e6c4c4d4083dc6b1fd1e2b8e8
JGuzman87 <jguzman913@gmail.com>
2025-12-17 22:05:07 -0500

    started adding tanstack query

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
-
+import { useQuery } from "@tanstack/react-query"
+import { supabase } from "@supabase/supabase-js";
 import Todo from './components/Todo';
 import './App.css'
 
 function App() {
 
+    const getList = async () => {
+      const {  error } = await supabase
+        .from("list")
+        .select('*');
+      if (error) {
+        alert(error.message);
+      }
+    };
+  
+
+  const {data, isLoading, error} = useQuery({queryKey: ['list'], queryFn: getList} );
+
+  if (isLoading) return <p>Loading...</p>
+  if (error) return <p>Error ocurred: {error.message}</p>
 
   return (
     <>
-    <Todo />
-
+    <Todo  />
+    <ul>
+      
+    </ul>
     </>
   )
 }
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { supabase } from "../lib/supabaseClient";
 
 const Todo = () => {
