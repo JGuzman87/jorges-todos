4539edb323c6db9ce582456d9766f9067d37d15f
JGuzman87 <jguzman913@gmail.com>
2025-12-17 21:11:55 -0500

    created supabase post logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react'
 
+import Todo from './components/Todo';
 import './App.css'
 
 function App() {
@@ -7,6 +7,7 @@ function App() {
 
   return (
     <>
+    <Todo />
 
     </>
   )
---
+++ src/components/Todo.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import { supabase } from "../lib/supabaseClient";
+
+const Todo = () => {
+  const [todo, setTodo] = useState("");
+
+  const addList = async () => {
+    const {  error } = await supabase
+      .from("list")
+      .insert([{ name: todo }])
+      .select();
+
+      if (error) {
+        alert(error.message)
+      } else {
+        console.log(todo);
+        setTodo('')
+      }
+  };
+
+  return (
+    <>
+      <div className="flex flex-col gap-2">
+        <input
+          className="p-1"
+          type="text"
+          placeholder="enter todo..."
+          onChange={(e) => setTodo(e.target.value)}
+          value={todo}
+        />
+        <button type="button " className="btn btn-primary" onClick={() => addList()}>
+          Submit
+        </button>
+      </div>
+
+      <div>
+        <ul></ul>
+      </div>
+    </>
+  );
+};
+
+export default Todo;
