58ba4c41690d5cad582c628b36aa8d7c503533d6
JGuzman87 <jguzman913@gmail.com>
2025-12-19 18:53:24 -0500

    added AnimatePresense

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import { motion } from "framer-motion";
-import { useQuery } from "@tanstack/react-query";
+import { motion, AnimatePresence } from "framer-motion";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { supabase } from "./lib/supabaseClient";
 import Todo from "./components/Todo";
 import "./App.css";
 
 function App() {
+  //useQueryClient() is necessary for list to re-fetch
 
+  const queryClient = useQueryClient();
 
+  //function to fetch from Supabase
   const getList = async () => {
     console.log("FETCHING FROM SERVER");
 
@@ -17,52 +20,59 @@ function App() {
     return data;
   };
 
+  // Fetches and caches the list data from Supabase using TanStack Query
+  // and provides loading state via isLoading
 
-
-  const deleteList = async (id) => {
-    console.log(id)
-    const { data, error } = await supabase
-      .from("list")
-      .delete()
-      .eq("id", id)
-      .select()
-      ;
-
-    if (error) {
-      throw new Error("unable to delete list");
-    }
-    console.log(data)
-    return data;
-  };
-
-  
-
-  const { data, isLoading, error } = useQuery({
+  const { data, isLoading } = useQuery({
     queryKey: ["list"],
     queryFn: getList,
   });
 
-  if (isLoading) return <p>Loading...</p>;
-  if (error) return <p>Error ocurred: {error.message}</p>;
+  //function to delete from Supabase
+  const deleteList = async (id) => {
+    const { data, error } = await supabase
+      .from("list")
+      .delete()
+      .eq("id", id)
+      .select();
+    if (error) {
+      throw new Error("unable to delete list");
+    }
+    console.log(data);
+    return data;
+  };
+
+  //mutates list data in order to delete
+  const { mutate } = useMutation({
+    mutationFn: (id) => deleteList(id),
+
+    onSuccess: () => {
+      queryClient.invalidateQueries(["list"]);
+    },
+  });
 
   return (
     <div className="flex flex-col gap-2">
       <Todo />
       <ul className="flex flex-col gap-2 w-md">
-        {data.map((list) => (
-          <motion.li
-            initial={{ opacity: 0, y: 20 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.5, ease: "easeInOut" }}
-            key={list.id}
-            className="flex justify-between bg-red-200 text-start p-2"
-          >
-            {list.name}
-            <button className="btn w-fit" onClick={() => deleteList(list.id)}>
-              Delete
-            </button>
-          </motion.li>
-        ))}
+        <AnimatePresence>
+          {!isLoading &&
+            data.map((list) => (
+              <motion.li
+                initial={{ opacity: 0, x: -200 }}
+                animate={{ opacity: 1, x: 0 }}
+                exit={{ opacity: 0, x: 200 }}
+                transition={{ duration: 0.5, ease: "easeInOut" }}
+                key={list.id}
+                className="flex justify-between bg-red-200 text-start p-2"
+              >
+                {list.name}
+                <button className="btn w-fit" onClick={() => mutate(list.id)}>
+                  Delete
+                </button>
+              </motion.li>
+            ))}
+        </AnimatePresence>
       </ul>
     </div>
   );
