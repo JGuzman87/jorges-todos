f2bcc11b5290d7fe289e5e65271a42df17c8505b
JGuzman87 <jguzman913@gmail.com>
2025-12-20 09:55:51 -0500

    adding supabase auth

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,51 +1,69 @@
-import { motion, AnimatePresence} from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { supabase } from "../lib/supabaseClient";
 
-import {useState} from 'react'
+import { useState } from "react";
 
 const LoginForm = () => {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
 
-    const [user, setUser] = useState({
-        email: '',
-        password: '',
-    });
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+    const { error } = await supabase
+      .from("users")
+      .insert([{ email: user.email, password: user.password }])
+      .select();
 
-        const { error} = await supabase
-        .from('user')
-        .insert([{email: user.email, password: user.password}])
-        .select()
-
-        if (error) {
-            throw new Error("Error adding user");
-
-        }else {
-            console.log(user)
-            setUser({email: '', password:""})
-        }
-      
+    if (error) {
+      throw new Error("Error adding user");
+    } else {
+        await supabase.auth.signInWithPassWord({
+            email: user.email,
+            password: user.password
+        })
+      console.log(user);
+      setUser({ email: "", password: "" });
     }
+  };
 
-    const handleChange = (e) => {
-        const {name, value} = e.target;
+  const handleChange = (e) => {
+    const { name, value } = e.target;
 
-        setUser(prev => ({...prev, [name]: value}))
-    }
+    setUser((prev) => ({ ...prev, [name]: value }));
+  };
 
   return (
     <>
-      <form onSubmit={handleSubmit} className='flex flex-col gap-4 md:w-1/2 items-center mt-50 self-center shadow-2xl p-2 rounded-lg'>
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col gap-4 md:w-1/2 items-center mt-50 self-center shadow-2xl p-2 rounded-lg"
+      >
         <label htmlFor="email">Email: </label>
-        <input type="text" name="email" value={user.email} placeholder='enter email...' onChange={handleChange}/>
+        <input
+          type="text"
+          name="email"
+          value={user.email}
+          placeholder="enter email..."
+          onChange={handleChange}
+        />
         <label htmlFor="password">Password: </label>
-        <input type="text" name="password" value={user.password} placeholder='enter password...' onChange={handleChange}/>
-        <button type="submit" className='btn btn-success'>Login</button>
+        <input
+          type="text"
+          name="password"
+          value={user.password}
+          placeholder="enter password..."
+          onChange={handleChange}
+        />
+        <button type="submit" className="btn btn-success">
+          Login
+        </button>
       </form>
     </>
   );
-}
+};
 
-export default LoginForm
+export default LoginForm;
